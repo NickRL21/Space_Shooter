fc32d6c837378ff6b7be85e4eeb17f7ae1448b66
Nick Lewis <nick@adlightning.com>
2019-03-05 02:08:51 -0800

    test site host

---
+++ docs/src/MyGame/Objects/EnemyLaser.js
@@ -0,0 +1,53 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function EnemyLaser(spriteSource, playerXform) 
+{
+    // source for the wing image
+    this.kSpriteSource = spriteSource;
+    this.valid = true; 
+    this.mSprite = new SpriteRenderable(this.kSpriteSource);
+    //325 490
+    this.mSprite.setElementPixelPositions(841, 854, 340, 377);
+    this.mSprite.getXform().setSize(1.5, 1.5);
+
+    this.mSprite.getXform().setRotationInDegree(playerXform.getRotationInDegree());
+    this.mSprite.getXform().setPosition(playerXform.getPosition()[0], playerXform.getPosition()[1]);
+  
+    Projectile.call(this, this.mSprite);
+    Projectile.prototype.setSpeed.call(this, 0.1);
+    Projectile.prototype.setDamage.call(this, 10);
+    
+};
+gEngine.Core.inheritPrototype(EnemyLaser, Projectile);
+
+EnemyLaser.prototype.draw = function (aCamera) 
+{
+   Projectile.prototype.draw.call(this, aCamera);
+    //GameObject.prototype.draw.call(this, aCamera);
+};
+
+
+EnemyLaser.prototype.update = function(enemies) 
+{   
+    var xform = this.mSprite.getXform();
+    xform.incXPosBy(0.5 * Math.cos(xform.getRotationInRad() + (Math.PI/2)));
+    xform.incYPosBy(0.5 * Math.sin(xform.getRotationInRad() + (Math.PI/2)));
+    for (var i = 0; i < enemies.length; ++i){
+        var box = enemies[i].getBBox();
+        var boxResult = box.containsPoint(this.mSprite.getXform().getXPos(), this.mSprite.getXform().getYPos());
+        if (boxResult){
+            if(this.valid){
+                enemies[i].hit(Projectile.prototype.getDamage.call(this));
+            }
+            this.valid = false;
+            return false;
+        }
+    }
+    Projectile.prototype.update.call(this);
+    return true;
+};
+
---
+++ docs/src/MyGame/Objects/PlayerShip.js
@@ -0,0 +1,125 @@
+/* File: PlayerShip.js 
+ *
+ * Creates and initializes a Minion object
+ * overrides the update function of GameObject to define
+ * simple sprite animation behavior behavior
+ */
+
+/*jslint node: true, vars: true */
+/*global gEngine: false, GameObject: false, TextureRenderable: false */
+/* find out more about jslint: http://www.jslint.com/help.html */
+
+"use strict";  // Operate in Strict mode such that variables must be declared before used!
+
+var kShipWidth = 6*0.5;
+var kShipHeight = 4.8*0.5;
+var kShipRandomSize = 5;
+
+function PlayerShip(spriteTexture, atX, atY, size) 
+{
+    var w = kShipWidth + size;
+    var h = kShipHeight + size;
+    
+    // create sprite renderable
+    this.mPlayerShip = new SpriteRenderable(spriteTexture);
+    this.mTexture = spriteTexture;
+    
+    // set position to the red ship
+    this.mPlayerShip.setElementPixelPositions(0,111,8,80);
+    this.mPlayerShip.setColor([1, 1, 1, 0]);
+    
+    // set the position and size
+    this.mPlayerShip.getXform().setPosition(atX, atY);
+    this.mPlayerShip.getXform().setSize(w, h);
+
+    GameObject.call(this, this.mPlayerShip);
+    
+    this.mIsAlive = true;
+    this.mLasers = [];
+    this.mHealth = 100;
+    this.mShield = new Shield(spriteTexture, this.mPlayerShip.getXform());
+    
+    
+    var r;
+    r = new RigidRectangle(this.getXform(), w, h);
+    var vx = (Math.random() - 0.5);
+    var vy = (Math.random() - 0.5);
+    var speed = 20 + Math.random() * 10;
+    r.setMass(0);
+    r.mDrawBound = false;
+    this.setRigidBody(r);
+    
+    //this is spawing as invisible?????
+    this.toggleDrawRenderable();
+    this.toggleDrawRigidShape();
+}
+gEngine.Core.inheritPrototype(PlayerShip, WASDObj);
+
+PlayerShip.prototype.hit = function(damage)
+{
+    this.mHealth -= damage;
+    if(this.mHealth <= 0)
+    {
+        this.mIsAlive = false;
+    }
+};
+
+PlayerShip.prototype.isAlive = function()
+{
+    return this.mIsAlive;
+};
+
+PlayerShip.prototype.draw = function (aCamera) 
+{
+    for(var i = 0; i < this.mLasers.length; i++)
+    {
+        this.mLasers[i].draw(aCamera);
+    }
+    GameObject.prototype.draw.call(this, aCamera);
+    this.mShield.draw(aCamera);
+};
+
+PlayerShip.prototype.update = function (aCamera, enemies) 
+{
+    GameObject.prototype.update.call(this);
+    
+    // get ship coordinates
+    var shipPos = this.mPlayerShip.getXform().getPosition();
+    var playerX = shipPos[0];
+    var playerY = shipPos[1];
+    
+    for(var i = 0; i < this.mLasers.length; i++)
+    {
+        if (!this.mLasers[i].update(enemies))
+        {
+            this.mLasers.splice(i, 1);
+        }
+    }
+    
+    this.mShield.update(this.mPlayerShip.getXform());
+    
+    this.keyControl();
+    
+    aCamera.setWCCenter(playerX, playerY);
+    
+    if(aCamera.isMouseInViewport())
+    {
+        var mouseX = aCamera.mouseWCX();
+        var mouseY = aCamera.mouseWCY();
+        var angleDegrees = Math.atan2(playerY - mouseY, playerX - mouseX) * 180 / Math.PI;
+        this.mPlayerShip.getXform().setRotationInDegree(angleDegrees + 90);
+    }
+    
+    if(gEngine.Input.isButtonClicked(gEngine.Input.mouseButton.Left))
+    {
+        var laser = new Laser(this.mTexture, this.mPlayerShip.getXform());
+        this.mLasers.push(laser);
+    }
+    
+    if(gEngine.Input.isKeyClicked(gEngine.Input.keys.E))
+    {
+        this.mShield.activate();
+    }
+    
+    return this.mIsAlive;
+};
